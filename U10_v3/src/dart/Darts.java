a5960fdf4ffa96b24ebec723f09dcf5a0e9a1781
Me <me@172.20.80.180>
2018-01-17 17:44:32 +0100

    jjy Signed-off-by: Me <me@172.20.80.180>

--- U10_v3/src/dart/Darts.java
+++ U10_v3/src/dart/Darts.java
@@ -40,7 +40,7 @@ public abstract class Darts implements IDarts {
 			MyFrame.Win.error("ERROR : We need more player for game. Player Count = 0");
 		}
 	}
-	// hallo
+	// hallo43
 	
 	
 	public boolean addPlayer(Player player){

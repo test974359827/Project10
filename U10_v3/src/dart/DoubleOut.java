b61c0745fbc38fc2d4ea6657f5e1c3cce0a8ea74
Shayan <shayandavarifard@yahoo.com>
2018-01-19 00:12:27 +0100

    as

--- U10_v3/src/dart/DoubleOut.java
+++ U10_v3/src/dart/DoubleOut.java
@@ -4,16 +4,59 @@ public class DoubleOut extends Darts{
 
 	
 	private int wp = 501 ; // Gewwinpunkte 
-	
-	
+	private int Point = 0 ;
 	
 	public DoubleOut(int PlayerCount){
 		super("Double Out",PlayerCount);
-		gameStart();
+		gameDoubleOut();
+	}
+	public void Point(Player Player){
+		Point = 0 ;
+		int[][] PlayerPoint = Player.getThrowDartValue();
+		for(int i = 0 ; i < PlayerPoint.length ; i++)
+			Point = PlayerPoint[i][0] * PlayerPoint[i][1] + Point;
+	}
+	public boolean ObGewinnt(){
+		if (wp == Point)
+			return true;
+		return false;
 	}
 	
-	
-	public void gameStart(){
+	public void gameDoubleOut(){
 		
+		while(! isOver()){
+			Player player = getPlayerByIndex(getActivePlayerNumber());
+			if(getLeftDarts() > 0){
+				int[] in = input();
+				throwDart(in[0],in[1]);
+				Point(player);
+				if(Point > wp){
+					int[] TD = { 0 , 0 };
+					switch (getLeftDarts()){
+					case 2 : 
+						player.setThrowDartValueByIndex(player.getThrowDartValue().length - 1 , TD);
+						player.setThrowDartValue(TD);
+						player.setThrowDartValue(TD);
+						break;
+					case 1 : 
+						player.setThrowDartValueByIndex(player.getThrowDartValue().length - 2 , TD);
+						player.setThrowDartValueByIndex(player.getThrowDartValue().length - 1 , TD);
+						player.setThrowDartValue(TD);
+						break;
+					case 0 : 
+						player.setThrowDartValueByIndex(player.getThrowDartValue().length - 3 , TD);
+						player.setThrowDartValueByIndex(player.getThrowDartValue().length - 2 , TD);
+						player.setThrowDartValueByIndex(player.getThrowDartValue().length - 1 , TD);
+						break;
+					}
+					nextPlayer();
+				}
+					
+				if()
+					endGame();
+			}
+			else if(getLeftDarts() == 0)
+				nextPlayer();
+		}
 	}
 }

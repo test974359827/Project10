b8e834a055f9ba2048d6b3d69595ba9566ce5f49
Me <me@mes-macbook-pro.local>
2018-01-19 21:02:56 +0100

    2

--- U10_v3/src/dart/Tactics.java
+++ U10_v3/src/dart/Tactics.java
@@ -10,7 +10,7 @@ public class Tactics extends Darts{
 			
 		}
 	public void Point(Player Player){
-		int[][] PlayerPoint = Player.getThrowDartValue();
+		PlayerPoint = Player.getThrowDartValue();
 	}
 	
 	public boolean gewonnen(){
@@ -28,14 +28,16 @@ public class Tactics extends Darts{
 		int wert=0;
 		for(int a=1; a<22; a++) {
 			wert=0;
-			for(int b=0; b<= PlayerPoint.length ; b++) {
+			for(int b=0; b< PlayerPoint.length ; b++) {
 				if(PlayerPoint[b][0]==a) {
 					wert= wert+PlayerPoint[b][1];
 					} if(PlayerPoint[b][0]==25){
 						wert= wert+PlayerPoint[b][1];	
-				}	
 				}
-			PlayerPointMerge[a][1]= wert;
+					PlayerPointMerge[a][1]= wert;
+					PlayerPointMerge[a][0]= a;
+				}
+			
 			}
 	for(int i= 9; i<= PlayerPointMerge.length; i++) {
 			if(PlayerPointMerge[i][0]==i+1 

cc0199d185e5bf0cb241afe9260c0ce649dda114
Me <me@mes-macbook-pro.local>
2018-01-20 21:04:39 +0100

    erts

--- U10_v3/src/dart/Tactics.java
+++ U10_v3/src/dart/Tactics.java
@@ -20,6 +20,7 @@ public class Tactics extends Darts{
 	 * die Punkte von Player wird mithilfe von Klasse Player in Array gespeichert.
 	 * @param Player 
 	 * kein Return
+	 * 
 	 */
 	public void Point(){
 		PlayerPoint = getPlayerByIndex(getActivePlayerNumber()).getThrowDartValue();

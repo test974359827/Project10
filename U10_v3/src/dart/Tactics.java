df0edcbf60c4047a168a2d80aa1acdf4ebf73d23
Shayan <shayandavarifard@yahoo.com>
2018-01-20 20:58:07 +0100

    a

--- U10_v3/src/dart/Tactics.java
+++ U10_v3/src/dart/Tactics.java
@@ -19,9 +19,9 @@ public class Tactics extends Darts{
 	/**
 	 * die Punkte von Player wird mithilfe von Klasse Player in Array gespeichert.
 	 * @param Player 
-	 */
-	public void Point(){
-		PlayerPoint = getPlayerByIndex(getActivePlayerNumber()).getThrowDartValue();
+	 */ 
+	public void Point(Player Player){
+		PlayerPoint = Player.getThrowDartValue();
 	}
 	
 	/**
@@ -39,34 +39,25 @@ public class Tactics extends Darts{
 	// Z.B.: 18 (Nummer)& 2(	Multiplikator) ,18 (Nummer)& 1(Multiplikator) => 18 (Nummer)& 3(Multiplikator)
 	// Algorithmus von Merge:
 		int wert=0;
-		System.out.println("1");
 		for(int a=1; a<22; a++) {
-			System.out.println("22");
 			wert=0;
 			for(int b=0; b< PlayerPoint.length ; b++) {
-				System.out.println("22");
 				if(PlayerPoint[b][0]==a) {
-					System.out.println("3");
 					wert= wert+PlayerPoint[b][1];
-					}
-				System.out.println("222");
-				if(PlayerPoint[b][0]==25){
-						System.out.println("4");
+					} if(PlayerPoint[b][0]==25){
 						wert= wert+PlayerPoint[b][1];	
 				}
 					PlayerPointMerge[a][1]= wert; // Multiplikator von der Zahl
 					PlayerPointMerge[a][0]= a;    // welche Nummer der Player getroffen hat
-					System.out.println("120");
+				
 				}
 			
 			}
-		System.out.println("2");
 	for(int i= 9; i< PlayerPointMerge.length; i++) { // entscheidet ob der Spieler gewonnen hat.
 			// wenn Player alle Zahlen von 10 bis 20 und 25 dreimal getroffen hat dann hat er gewonnen.
 		if(PlayerPointMerge[i][0]==i+1 
 					&& ((PlayerPointMerge[i][0])*(PlayerPointMerge[i][1])
 					> 3*(PlayerPointMerge[i][0]))) {
-			System.out.println("6");
 				return true;	
 			}
 		}
@@ -78,10 +69,11 @@ public class Tactics extends Darts{
  */
 	public void gameTactics() {
 		while(! isOver()){
+			Player player = getPlayerByIndex(getActivePlayerNumber());
 			if(getLeftDarts() > 0){ // so lange der Player Darts übrig hat, kann er weiterspielen
 				int[] in = input();
 				throwDart(in[0],in[1]);
-				Point();
+				Point(player);
 				if(gewonnen()) { // wenn Player gewonnen hat dann endGame.
 					setGameWinner(true);
 					endGame();

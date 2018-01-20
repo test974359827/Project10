5fd8134669ed9a791ced83f34960c03f792479a9
ArianTashakkor <ArianTashakkor@ArianPC>
2018-01-20 22:31:37 +0100

    myFrame NEW

--- U10_v3/src/UI/MyFrame.java
+++ U10_v3/src/UI/MyFrame.java
@@ -93,6 +93,7 @@ public class MyFrame {
 				if(e.getKeyCode() == KeyEvent.VK_ENTER) start();
 			}
 		});
+		
 		rdbtnDoubleOut.setBounds(124, 67, 113, 29);
 		rdbtnDoubleOut.setSelected(true);
 		frmDart.getContentPane().add(rdbtnDoubleOut); // Spielmodus DoubleOut

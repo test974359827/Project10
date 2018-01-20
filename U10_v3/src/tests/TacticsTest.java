1002e7cdbc0083b4c90fe0dbbd74064d016bd351
Me <me@mes-macbook-pro.local>
2018-01-20 19:12:49 +0100

    Test

--- U10_v3/src/tests/TacticsTest.java
+++ U10_v3/src/tests/TacticsTest.java
@@ -1,5 +1,61 @@
 package tests;
 
-public class TacticsTest {
+import static org.junit.Assert.assertEquals;
 
-}
+import org.junit.Test;
+
+import dart.Tactics;
+/**
+ * 
+ * @author Mohammadrahim Masoumi
+ *
+ */
+public class TacticsTest {
+	
+	
+		Tactics Mo = new Tactics(2);
+		
+	 @Test
+	 public void GameStarting(){
+		 
+		 assertEquals(2,Mo.getPlayerCount());
+		 assertEquals("Tactics",Mo.getGamemode());
+	
+	
+		 
+		 
+		 Mo.throwDart(1, 2);
+		 Mo.throwDart(11, 3);
+		 Mo.throwDart(12, 2);
+		 Mo.throwDart(20, 2);
+		 Mo.throwDart(17, 2);
+		 Mo.throwDart(14, 3);
+		 Mo.throwDart(9, 2);
+		 Mo.throwDart(18, 2);
+		 Mo.throwDart(19, 2);
+		 Mo.throwDart(25, 2);
+		 Mo.throwDart(10, 2);
+		 Mo.throwDart(16, 3);
+		 Mo.throwDart(12, 1);
+		 Mo.throwDart(10, 1);
+		 Mo.throwDart(25, 2);
+		 Mo.throwDart(17, 1);
+		 Mo.throwDart(19, 1);
+		 Mo.throwDart(20, 2);
+		 Mo.throwDart(18, 1);
+		 Mo.throwDart(13, 3);
+		 Mo.throwDart(15, 1);
+		 Mo.throwDart(15, 2);
+		 
+		 Mo.gewonnen();
+		 
+		 
+		 
+		 assertEquals(true, Mo.gewonnen());
+	 }
+	 
+
+		
+	}
+
+
